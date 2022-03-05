f424061287828413895a968cfbf264f9203b2dd6
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-05 17:28:00 -0600

    add location method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def geo(city)
+    if Geolocation.where("city = ?", city).exists?
+      @location = Geolocation.find_by(city: city)
+    else
+      @location = LocationFacade.lat_lng(city)
+      Geolocation.create(@location.as_json)
+    end
+  end
 end
