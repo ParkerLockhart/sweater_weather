161661f50667fa4ab738c3b2267fa7ba1c4b8d25
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-05 17:27:37 -0600

    forecast route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get '/forecast', to: 'forecast#index' 
+    end
+  end
 end
