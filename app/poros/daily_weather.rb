bb23f1e74fefca852c7958f861a173432f03bab8
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-05 15:45:10 -0600

    daily weather poro class

---
+++ app/poros/daily_weather.rb
@@ -0,0 +1,13 @@
+class DailyWeather
+  attr_reader :dt, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon
+
+  def initialize(data)
+    @dt = Time.at(data[:dt]).to_datetime
+    @sunrise = Time.at(data[:dt]).to_datetime
+    @sunset = Time.at(data[:dt]).to_datetime
+    @max_temp = data[:temp][:max]
+    @min_temp = data[:temp][:min]
+    @conditions = data[:weather].first[:description]
+    @icon = data[:weather].first[:icon]
+  end
+end
