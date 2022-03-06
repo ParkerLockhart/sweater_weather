d032f4e4214070c1821aa1a6f850e01ec62c77ed
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-05 23:05:05 -0600

    background request route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#index' 
+      get '/forecast', to: 'forecast#index'
+      get '/backgrounds', to: 'photos#show' 
     end
   end
 end
