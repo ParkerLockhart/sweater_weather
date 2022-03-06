c261c86611a2ae5d8219e6cf7b81abf53d34387c
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-06 14:52:38 -0600

    users post route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
-      get '/backgrounds', to: 'photos#show' 
+      get '/backgrounds', to: 'photos#show'
+      post '/users', to: 'users#create'
+      # get '/users/:id', to: 'users#show'
+      # post '/sessions', to: 'sessions#create'
+      # get '/sessions/:id', to: 'sessions#show'
     end
   end
 end
