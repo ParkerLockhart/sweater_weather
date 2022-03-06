72e5aa13a0815558fbc8f986ebda1777cbc682ca
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-06 14:52:13 -0600

    remove api_key presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_secure_token :api_key
   validates_uniqueness_of :email
   validates_uniqueness_of :api_key
-  validates_presence_of :email, :password_digest, :api_key
+  validates_presence_of :email, :password_digest
 
 end
