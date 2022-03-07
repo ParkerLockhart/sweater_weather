c6e4a882a100a350f62297574f7d28dc352d7287
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-07 11:30:08 -0600

    Add serializer args

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::BooksController < ApplicationController
     location = geo(params[:location])
     weather = WeatherFacade.current_weather(location.lat, location.lng)
     books = BookFacade.book_search(params[:location], params[:quantity])
-    BookSerializer.book_search(weather, books)
+    BookSerializer.book_search(weather, books, location)
   end
 end
