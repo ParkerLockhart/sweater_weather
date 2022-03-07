9ab60f0f20b10cc2f88595d6d8a56c541205bd86
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-07 11:39:43 -0600

    adjust book facade test for changes

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe BookFacade do
   describe 'book_search' do
     it 'returns books about city' do
       VCR.use_cassette('denver_books') do
-        books = BookFacade.book_search('denver', 10)
+        facade = BookFacade.book_search('denver', 10)
+        total_results = facade.first
+        books = facade.last
 
+        expect(total_results).to eq(52)
         expect(books.count).to eq(10)
 
         books.each do |book|
