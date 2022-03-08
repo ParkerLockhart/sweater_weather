6f2483d9a2a6665c35e4da2151753f00fa556e1e
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-08 17:25:04 -0600

    refactor weather format for sad path

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -6,6 +6,6 @@ class Roadtrip
     @start_city = data[:start_city]
     @end_city = data[:end_city]
     @travel_time = data[:travel_time]
-    @weather_at_eta = { temperature: data[:weather][:temp], conditions: data[:weather][:weather].first[:description] }
+    @weather_at_eta = data[:weather]
   end
 end
