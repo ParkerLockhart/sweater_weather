06ce777ca899ea7d415aab8e2a5ab313b9a7ffb3
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-08 14:18:50 -0600

    add require api key authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,17 @@ class ApplicationController < ActionController::API
       Geolocation.create(@location.as_json)
     end
   end
+
+  def require_user
+    if !authorized_user
+      render json: {
+        error: "Invalid api_key.",
+      },
+      status: :unauthorized
+    end
+  end
+
+  def authorized_user
+    User.find_by api_key: params[:api_key]
+  end
 end
