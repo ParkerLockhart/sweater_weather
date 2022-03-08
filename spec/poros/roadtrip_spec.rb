13b24f31202f5442a5e797effac20afeb4f6b7fe
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-08 14:10:48 -0600

    roadtrip poro test

--- spec/poros/roadtrip_spec.rb
+++ spec/poros/roadtrip_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Roadtrip do
   let!(:start_city) {'san antonio, tx'}
   let!(:end_city) {'denver, co'}
   let!(:travel_time) {'14hrs, 21mins'}
-  let!(:weather) { { temp: 59.4, weather: { description: 'scattered showers'} } }
+  let!(:weather) { { temp: 59.4, weather: [{ description: 'scattered showers'}] } }
   let!(:data) { {start_city: start_city, end_city: end_city, travel_time: travel_time, weather: weather} }
   let!(:roadtrip) {Roadtrip.new(data)}
 
@@ -13,6 +13,7 @@ RSpec.describe Roadtrip do
   end
 
   it 'has attributes' do
+    expect(roadtrip.id).to eq(nil)
     expect(roadtrip.start_city).to eq('san antonio, tx')
     expect(roadtrip.end_city).to eq('denver, co')
     expect(roadtrip.travel_time).to eq('14hrs, 21mins')
