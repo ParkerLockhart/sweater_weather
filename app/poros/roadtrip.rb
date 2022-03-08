55481e57028b48b82664be193ff607cb795502bb
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-08 14:11:04 -0600

    roadtrip poro

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -1,10 +1,11 @@
 class Roadtrip
-  attr_reader :start_city, :end_city, :travel_time, :weather_at_eta
+  attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
   def initialize(data)
+    @id = nil
     @start_city = data[:start_city]
     @end_city = data[:end_city]
     @travel_time = data[:travel_time]
-    @weather_at_eta = { temperature: data[:weather][:temp], conditions: data[:weather][:weather][:description] }
+    @weather_at_eta = { temperature: data[:weather][:temp], conditions: data[:weather][:weather].first[:description] }
   end
 end
